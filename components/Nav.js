b28bf6d693664610777488e02309bd187efe3789
Aakash Srivastav <akhsri21@gmail.com>
2020-11-20 21:52:06 +0530

    fix: changed class to className

--- components/Nav.js
+++ components/Nav.js
@@ -2,9 +2,9 @@ import ActiveLink from './ActiveLink'
 
 export default function Nav() {
     return (
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-black">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-black">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
