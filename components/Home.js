2c5af14ab36084c1b6b5f153419e87ffbbf0ea34
Aakash Srivastav <akhsri21@gmail.com>
2020-11-21 22:31:09 +0530

    Added tap anywhere text and animation

--- components/Home.js
+++ components/Home.js
@@ -1,25 +1,29 @@
 import styles from '../styles/Home.module.css'
+import { Fragment } from 'react';
 
 export default function Home() {
 
     return (
-        <div className={`${styles.container} container-fluid`}>
-            <div>
-                <h1 className={styles.homepageheading}>
-                    <div className={styles.hoverlink} id={styles.homehead1}>
-                        <div>Hello.</div>
-                        <span><a href="/about">About</a></span>
-                    </div>
-                    <div style={{ color: "red" }} className={styles.hoverlink} id={styles.homehead2}>
-                        <div>I am</div>
-                        <span><a href="#">Work</a></span>
-                    </div>
-                    <div style={{ color: "red" }} className={styles.hoverlink} id={styles.homehead3}>
-                        <div>Aakash</div>
-                        <span><a href="/contact">Contact</a></span>
-                    </div>
-                </h1>
+        <Fragment>
+            <div className={`${styles.container} container-fluid`}>
+                <div>
+                    <h1 className={styles.homepageheading}>
+                        <div className={styles.hoverlink} id={styles.homehead1}>
+                            <div>Hello.</div>
+                            <span><a href="/about">About</a></span>
+                        </div>
+                        <div style={{ color: "red" }} className={styles.hoverlink} id={styles.homehead2}>
+                            <div>I am</div>
+                            <span><a href="#">Work</a></span>
+                        </div>
+                        <div style={{ color: "red" }} className={styles.hoverlink} id={styles.homehead3}>
+                            <div>Aakash</div>
+                            <span><a href="/contact">Contact</a></span>
+                        </div>
+                    </h1>
+                </div>
+                <div className={styles.tapindicaton}>Tap anywhere</div>
             </div>
-        </div>
+        </Fragment>
     )
 }
