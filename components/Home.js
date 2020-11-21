6de1753b477c28e3131451c67806f0ebe78a2362
Aakash Srivastav <akhsri21@gmail.com>
2020-11-21 19:39:13 +0530

    Added links to anchor tags on homepage

--- components/Home.js
+++ components/Home.js
@@ -6,17 +6,17 @@ export default function Home() {
         <div className={`${styles.container} container-fluid`}>
             <div>
                 <h1 className={styles.homepageheading}>
-                    <div className={styles.hoverlink}>
+                    <div className={styles.hoverlink} id={styles.homehead1}>
                         <div>Hello.</div>
-                        <span><a href="#">About</a></span>
+                        <span><a href="/about">About</a></span>
                     </div>
-                    <div style={{ color: "red" }} className={styles.hoverlink}>
+                    <div style={{ color: "red" }} className={styles.hoverlink} id={styles.homehead2}>
                         <div>I am</div>
                         <span><a href="#">Work</a></span>
                     </div>
-                    <div style={{ color: "red" }} className={styles.hoverlink}>
+                    <div style={{ color: "red" }} className={styles.hoverlink} id={styles.homehead3}>
                         <div>Aakash</div>
-                        <span><a href="#">Contact</a></span>
+                        <span><a href="/contact">Contact</a></span>
                     </div>
                 </h1>
             </div>
